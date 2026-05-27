72338524aae6e5fcc737d290508107233c834f6a
adminnooracademy <admin.nooracademy@gmail.com>
2026-05-27 20:56:21 +0700

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.PMB_CONFIG = {
   // Isi dengan Web App URL Google Apps Script setelah deploy.
-  sheetsWebAppUrl: "",
+  sheetsWebAppUrl: "https://script.google.com/macros/s/AKfycbwIPTztQzhLxMe_FTGBCm0apZjqvV_uMIeuES4NeK1QgWTQGKxq9x_PeVkRlnCljX1V/exec",
 
   // Samakan dengan password admin di database/users.js dan Script Property ADMIN_PASSWORD di Google Apps Script.
   // Catatan: karena website statis, nilai ini tetap bisa dibaca dari source code.
@@ -12,7 +12,7 @@ window.PMB_CONFIG = {
     micOptional: true,
     cameraRequired: false,
     micRequired: false,
-    fullscreenRequired: true,
+    fullscreenRequired: false,
 
     // false = paling aman untuk 500 peserta, hanya hasil akhir yang dikirim.
     // true = kirim event penting ke Google Sheets; gunakan hanya jika Sheets sudah kuat dan peserta tidak terlalu banyak.
