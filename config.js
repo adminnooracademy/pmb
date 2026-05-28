3615978a94381a0dbb44d75bdc4d3f2dc4161d05
adminnooracademy <admin.nooracademy@gmail.com>
2026-05-28 20:18:58 +0700

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.PMB_CONFIG = {
   // Isi dengan Web App URL Google Apps Script setelah deploy.
-  sheetsWebAppUrl: "",
+  sheetsWebAppUrl: "https://script.google.com/macros/s/AKfycbzsL_XYU1bunFXr6sJ9DVpz1uJC4fN0JHddBM8asDckSkfn_yVwPSGMXkgwEI206dX7iw/exec",
 
   // Samakan dengan password admin di database/users.js dan Script Property ADMIN_PASSWORD di Google Apps Script.
   // Catatan: karena website statis, nilai ini tetap bisa dibaca dari source code.
